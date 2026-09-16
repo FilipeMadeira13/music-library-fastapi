6e8e3f0803cd890d15bcae927587c306f6e29085
FilipeMadeira13 <cfilipemadeira@gmail.com>
2026-09-16 20:13:15 -0300

    Adiciona validação de ano ao modelo Artist

--- app/models/artist.py
+++ app/models/artist.py
@@ -1,11 +1,10 @@
-from datetime import date
-from typing import Optional
+from datetime import datetime
+from typing import Annotated, Optional
 
-from pydantic import BaseModel
-
+from pydantic import BaseModel, Field
 
 class Artist(BaseModel):
     id: int
     name: str
     country: Optional[str] = None
-    formed_in: Optional[date] = None
+    formed_in: Annotated[Optional[int], Field(le=datetime.now().year)]
