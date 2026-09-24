cca1859f9be26c84730b65428e10d3deb783dab6
FilipeMadeira13 <cfilipemadeira@gmail.com>
2026-09-24 17:23:00 -0300

    Cria a fixture client

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+from fastapi.testclient import TestClient
 
 from app.database.local import LocalDatabase
 
@@ -6,3 +7,13 @@ from app.database.local import LocalDatabase
 @pytest.fixture
 def database(tmp_path):
     return LocalDatabase(tmp_path / "test_music_library.db")
+
+
+@pytest.fixture
+def client(tmp_path, monkeypatch):
+    monkeypatch.chdir(tmp_path)
+
+    from app.main import app
+
+    with TestClient(app) as test_client:
+        yield test_client
